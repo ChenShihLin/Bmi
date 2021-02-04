3684997baf6535372fadb407af126799dc39630a
cswilly9015 <cswilly9015@gmail.com>
2021-02-04 16:54:56 +0800

    Activity 類別活用，輸入方塊、按鈕元件互動與運算

--- app/src/main/java/com/cswilly9015/MainActivity.java
+++ app/src/main/java/com/cswilly9015/MainActivity.java
@@ -3,12 +3,26 @@ package com.cswilly9015;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_linear);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void bmi(View view) {
+        EditText edWeight = findViewById(R.id.ed_w);
+        EditText edHeight = findViewById(R.id.ed_h);
+        String w = edWeight.getText().toString();
+        String h = edHeight.getText().toString();
+        float weight = Float.parseFloat(w);
+        float height = Float.parseFloat(h);
+        float bmi = weight / (height * height);
+        Log.d("MainActivity", "BMI : " + bmi);
     }
 }
