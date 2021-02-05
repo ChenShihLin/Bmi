8f77a0de02eaaeedc5ed303690849cd824a2c3ac
cswilly9015 <cswilly9015@gmail.com>
2021-02-05 16:43:35 +0800

    抽取字串資源並讓 APP 擁有多國語言，實務案例

--- app/src/main/java/com/cswilly9015/MainActivity.java
+++ app/src/main/java/com/cswilly9015/MainActivity.java
@@ -33,9 +33,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 new AlertDialog.Builder(MainActivity.this)
-                        .setTitle("Help")
-                        .setMessage("Body mass index (BMI) is a value derived from the mass (weight) and height of a person.")
-                        .setPositiveButton("OK", null)
+                        .setTitle(R.string.help)
+                        .setMessage(getString(R.string.bmi_info))
+                        .setPositiveButton(R.string.ok, null)
                         .show();
             }
         });
@@ -58,15 +58,15 @@ public class MainActivity extends AppCompatActivity {
         float bmi = weight / (height * height);
 
         Log.d("MainActivity", "BMI : " + bmi);
-        Toast.makeText(this, "Your BMI is :" + bmi, Toast.LENGTH_LONG).show();
+        Toast.makeText(this, getString(R.string.your_bmi_is) + bmi, Toast.LENGTH_LONG).show();
 
 //        result.setText("Your BMI is :" + bmi);
-        binding.result.setText("Your BMI is : " + bmi);
+        binding.result.setText(getString(R.string.your_bmi_is) + bmi);
 
         new AlertDialog.Builder(this)
                 .setTitle("BMI")
-                .setMessage("Your BMI is : " + bmi)
-                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                .setMessage(getString(R.string.your_bmi_is) + bmi)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         binding.weight.setText("");
