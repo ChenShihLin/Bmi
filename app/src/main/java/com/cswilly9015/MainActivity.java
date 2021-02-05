b9dbc328fadc8c0e917a8dd1f070b218c3c1b6c5
cswilly9015 <cswilly9015@gmail.com>
2021-02-05 15:52:53 +0800

    新版 View Binding 設計方法

--- app/src/main/java/com/cswilly9015/MainActivity.java
+++ app/src/main/java/com/cswilly9015/MainActivity.java
@@ -9,34 +9,46 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.cswilly9015.databinding.ActivityMainBinding;
 
-    private EditText edWeight;
-    private EditText edHeight;
-    private TextView result;
+public class MainActivity extends AppCompatActivity {
+//    private EditText edWeight;
+//    private EditText edHeight;
+//    private TextView result;
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        findViews();
+
+//        setContentView(R.layout.activity_main);
+//        findViews();
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 
-    private void findViews() {
-        edWeight = findViewById(R.id.ed_w);
-        edHeight = findViewById(R.id.ed_h);
-        result = findViewById(R.id.result);
-    }
+//    private void findViews() {
+//        edWeight = findViewById(R.id.weight);
+//        edHeight = findViewById(R.id.height);
+//        result = findViewById(R.id.result);
+//    }
 
     public void bmi(View view) {
-        String w = edWeight.getText().toString();
-        String h = edHeight.getText().toString();
+//        String w = edWeight.getText().toString();
+        String w = binding.weight.getText().toString();
+//        String h = edHeight.getText().toString();
+        String h = binding.height.getText().toString();
+
         float weight = Float.parseFloat(w);
         float height = Float.parseFloat(h);
         float bmi = weight / (height * height);
 
         Log.d("MainActivity", "BMI : " + bmi);
         Toast.makeText(this, "Your BMI is :" + bmi, Toast.LENGTH_LONG).show();
-        result.setText("Your BMI is :" + bmi);
+
+//        result.setText("Your BMI is :" + bmi);
+        binding.result.setText("Your BMI is : " + bmi);
     }
 }
